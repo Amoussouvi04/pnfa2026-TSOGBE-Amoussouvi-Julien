66e3a5dd76230223a492426578cc09841f360f67
Amoussouvi04 <tsogbejulien@gmail.com>
2026-09-05 11:36:42 +0000

    docs(journaux du 01 à 03 septembre 2026)

---
+++ .vscode/04 09 26.py
@@ -0,0 +1,37 @@
+# c'est pour connaitre le niveau de l'élève
+"""niveau = "maitrise l'imprimente 3D"
+if niveau == "débutant":
+     print("accesà l'impression uniquement")
+elif niveau == "intermediaire":
+     print("acces laser + perceuse")
+else:
+     print("Accès à toutes les machines")
+   """  
+
+        
+        
+        
+  # repeter un nombre de fois précis
+"""for i in range(5):
+    print("découpe piece n°",i)
+for i in range(1, 11):
+    print(i)
+      #machine =["laser", "cnc", "imprimante 3D"]
+      #for machine in machines:
+      #print(machine)
+    """
+"""stock = 5
+while stock > 0:
+    print (f"pieces restantes : {stock}")
+    stock -= 1
+    print("stock épuisé")
+       """
+
+"""élève = ["un", "deux", "trois", "quatre"]
+print(élève[0:2])
+élève.remove("trois")
+print(élève[0:])
+"""
+dimensions = (300, 200)
+
+        
